06411bbd349477ed9540ef04e8efc6ceae579726
Yevhenii Spivak <Yevhenii.spivak@gmail.com>
2020-06-15 17:17:58 +0300

    New

--- qwerty6.py
+++ qwerty6.py
@@ -1,9 +1,8 @@
 x = int(input())
 h = int(input())
 m = int(input())
-H = (x + h * 60 + m) // 60
-aM = x + h * 60 + m
-bM = aM // 60
-M = aM - bM * 60
+y = (x + h * 60 + m)
+H = y // 60
+M = y - H * 60
 print(H)
 print(M)
