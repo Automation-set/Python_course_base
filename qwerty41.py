99cedc0557ba4cb4471aa8912b42e7314b13dc4a
Yevhenii Spivak <Yevhenii.spivak@gmail.com>
2020-07-09 17:26:50 +0300

    New

---
+++ qwerty41.py
@@ -0,0 +1,15 @@
+# def min2(a, b):
+#     if a <= b:
+#         return a
+#     else:
+#         return b
+#
+# m = min2(42, 30)
+# print(m)
+# m = min2(min2(33, 67), 45)
+# print(m)
+
+def f(n):
+    return n * 10 + 5
+
+print(f(f(f(10))))
